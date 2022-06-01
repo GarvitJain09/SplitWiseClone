24bcc0d555fa14212480832680fb52540fdfe051
Garvit Jain <garvitjain09@gmail.com>
2022-06-02 01:47:24 +0530

    Delete splitwiseclone directory

--- splitwiseclone/src/reducer/index.js
+++
@@ -1,9 +0,0 @@
-import addFriend from "./addfriend";
-import amount from "./amount";
-import { combineReducers } from "redux";
-
-const rootReducers = combineReducers({
-  addFriend
-});
-
-export default rootReducers;
--- splitwiseclone/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { applyMiddleware, createStore } from "redux";
-import thunk from "redux-thunk";
-
-import rootReducers from "../reducer/index";
-
-const store = createStore(rootReducers, applyMiddleware(thunk));
-
-export default store;
